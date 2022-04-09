d126b206a43914cc72ac226a6da3bd717a77c52e
Ismail <95064612+double0double1@users.noreply.github.com>
2022-04-10 05:10:16 +0530

    Test - 2

--- js/script.js
+++ js/script.js
@@ -11,3 +11,26 @@ function hide() {
   popup.style.visibility = "hidden"
   // popup.classList.toggle("show");
 }
+
+
+
+const form = document.getElementById("signupform");
+// console.log(form)
+form.addEventListener('submit',function(event) {
+    if (!validatepwd()) {
+        event.preventDefault();
+    }  
+
+})
+
+function validatepwd() {
+  // console.log("yes")
+  let pwd = document.getElementById("userPass").value;
+  let pwdrpt = document.getElementById("userPassRepeat").value;
+
+  if (pwd != pwdrpt) {
+      alert("Password and Repeat Password doesn't match");
+      return false;
+  }
+  return true;
+}
