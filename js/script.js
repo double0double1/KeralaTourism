706d1c2512499b07c64675268e24a1255fc1a948
Ismail <95064612+double0double1@users.noreply.github.com>
2022-04-10 04:30:23 +0530

    test for login notifs

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Popup Toggle
+function show() {
+    var popup = document.getElementById("myPopup");
+    popup.style.visibility = "visible"
+    // popup.classList.toggle("show");
+  }
+
+  // Popup Toggle
+function hide() {
+  var popup = document.getElementById("myPopup");
+  popup.style.visibility = "hidden"
+  // popup.classList.toggle("show");
+}
